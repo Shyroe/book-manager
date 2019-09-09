a8f2307913ba411535550b2d08f71db28454fb75
Cesar Cachay <cesarcachay1996@gmail.com>
2019-09-08 19:41:27 -0500

    Add routing and create basic components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
+import Header from "./components/Header";
+import Products from "./components/Products";
+import EditProduct from "./components/EditProduct";
+import NewProduct from "./components/NewProduct";
+
 function App() {
   return (
-    <Provider store={store}>
-      <p>Crud with Redux</p>
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/products/new" component={NewProduct} />
+            <Route exact path="/products/edit/:id" component={EditProduct} />
+          </Switch>
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
