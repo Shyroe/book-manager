e4ecc335d6156119d5039ce1e70ec3aac5bb3d00
Cesar Cachay <cesarcachay1996@gmail.com>
2019-09-08 20:44:41 -0500

    Add list of products & form

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,9 +2,22 @@ import React from "react";
 
 const Products = () => {
   return (
-    <div>
-      <h1>LIST OF PRODUCTS....!</h1>
-    </div>
+    <>
+    <h2 className="text-center my-5">Product List</h2>
+
+    <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Actions</th>
+            </tr>   
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</>
   );
 };
 
